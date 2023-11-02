e013a7b727ff1dcf194cf778772c52961d1a23bd
carlocodes <Gian Uy>
2023-11-02 11:21:47 +0800

    initial commit

---
+++ src/main/java/com/carlocodes/clipped/ClippedApplication.java
@@ -0,0 +1,13 @@
+package com.carlocodes.clipped;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClippedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClippedApplication.class, args);
+	}
+
+}
