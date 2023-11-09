daeca208e7cdea40efb34687da3e4d7cdc65085e
carlocodes <Gian Uy>
2023-11-09 18:31:25 +0800

    issue-21 - exception classes

---
+++ src/main/java/com/carlocodes/clipped/exceptions/ClippedException.java
@@ -0,0 +1,12 @@
+package com.carlocodes.clipped.exceptions;
+
+public class ClippedException extends Exception {
+
+    public ClippedException(String message) {
+        super(message);
+    }
+
+    public ClippedException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
