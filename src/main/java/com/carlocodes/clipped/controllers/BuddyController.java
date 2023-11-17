2bee876b87abe4d396c7688de3df8941a9180ff7
carlocodes <Gian Uy>
2023-11-17 23:00:49 +0800

    issue-14 - get pending buddy requests endpoint

--- src/main/java/com/carlocodes/clipped/controllers/BuddyController.java
+++ src/main/java/com/carlocodes/clipped/controllers/BuddyController.java
@@ -1,15 +1,20 @@
 package com.carlocodes.clipped.controllers;
 
+import com.carlocodes.clipped.dtos.BuddyDto;
 import com.carlocodes.clipped.dtos.BuddyRequestDto;
 import com.carlocodes.clipped.exceptions.ClippedException;
 import com.carlocodes.clipped.services.BuddyService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Set;
 
 @RestController
 @RequestMapping("/buddy")
@@ -42,5 +47,10 @@ public class BuddyController {
     public ResponseEntity<String> removeBuddy(@RequestBody BuddyRequestDto buddyRequestDto) throws ClippedException {
         buddyService.removeBuddy(buddyRequestDto);
         return ResponseEntity.ok("Buddy removed!");
+    }
+
+    @GetMapping("/get-pending-buddy-requests/user/{id}")
+    public ResponseEntity<Set<BuddyDto>> getPendingBuddyRequests(@PathVariable long id) throws ClippedException {
+        return ResponseEntity.ok(buddyService.getPendingBuddyRequests(id));
     }
 }
--- src/main/java/com/carlocodes/clipped/repositories/BuddyRepository.java
+++ src/main/java/com/carlocodes/clipped/repositories/BuddyRepository.java
@@ -5,7 +5,9 @@ import com.carlocodes.clipped.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
+import java.util.Set;
 
 @Repository
 public interface BuddyRepository extends JpaRepository<Buddy, Long> {
@@ -16,4 +18,6 @@ public interface BuddyRepository extends JpaRepository<Buddy, Long> {
     Optional<Buddy> findBySenderAndReceiverAndAcceptedIsNull(User sender, User receiver);
 
     Optional<Buddy> findBySenderAndReceiverAndAcceptedIsTrue(User sender, User receiver);
+
+    Set<Buddy> findByReceiverAndAcceptedIsNull(User user);
 }
